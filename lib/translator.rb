f7caa4b3a66553e506906b69ee54998cd5ca2f74
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-31 02:00:45 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
 # require modules here
+require 'yaml'
 
-def load_library
+def load_library(path)
   # code goes here
+  translations = {}
+  emoticons    = YAML.load_file(path)
+  
+  emoticons.each do | name, meaning |
+    translations[name]            = {}
+    translations[name][:english]  = meaning[0]
+    translations[name][:japanese] = meaning[1]
+  end
+  
+  translations
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
+  puts emoticon
+  load_library(path).each do | translation, meaning |
+    if meaning[:english] == emoticon
+      return meaning[:japanese]
+    end
+  end
+  
+  'Sorry, that emoticon was not found'
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
+  load_library(path).each do | translation, meaning |
+    if meaning[:japanese] == emoticon
+      return translation
+    end
+  end
+  
+  'Sorry, that emoticon was not found'
 end
